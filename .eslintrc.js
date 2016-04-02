f00ddb745c7def6467a376e150deade977ff23f2
Tim Dang <timothy.dang@gmail.com>
2016-04-02 15:50:12 -0700

    (fix) cosmetic fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,9 @@ module.exports = {
     "no-undef": 0,
     "max-len": 0,
     "comma-dangle": [2, "never"],
-    "no-console": 0
+    "no-console": 0,
+    "func-names": 0,
+    "space-before-function-paren": ["error", "never"]
   },
   "ecmaFeatures": {
     "arrowFunctions": true
