56ab1f955d3a70cd365329d626850867313d2f06
Timothy Dang <timothy.dang@gmail.com>
2016-10-15 23:34:00 -0700

    Minor code formatting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,12 @@
 module.exports = {
-  "extends": "airbnb",
-
-  "rules": {
-    "no-undef": 0,
-    "max-len": 0,
-    "comma-dangle": [2, "never"],
-    "no-console": 0,
-    "func-names": 0,
-    "space-before-function-paren": ["error", "never"]
-  },
-  "ecmaFeatures": {
-    "arrowFunctions": true
+  'extends': 'standard',
+  'installedESLint': true,
+  'plugins': [
+    'standard',
+    'promise'
+  ],
+  'rules': {
+    'space-before-function-paren': 0,
+    'no-undef': 0
   }
 };
